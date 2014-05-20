9a161502438c06f3f6d33c46e7551b39991d4c39
Michel Hognerud <leyou.m@gmail.com>
2014-05-20 15:09:56 +0930

    fixing travis

---
+++ Tests/Classes/Newsi18n.php
@@ -0,0 +1,19 @@
+<?php
+namespace Asgard\Core\Tests\Classes;
+
+class Newsi18n extends \Asgard\Entity\Entity {
+	public static function definition(\Asgard\Entity\EntityDefinition $definition) {
+		$definition->properties = array(
+			'title' => array(
+				'i18n' => true,
+			),
+			'content'
+		);
+
+		$definition->addProperty('another_property');
+	}
+
+	public function __toString() {
+		return $this->title;
+	}
+}
